6d8a930da63496f1b2303c9de1a3f9224e6cc1c3
Jose Sulbaran <twjasa@gmail.com>
2023-04-27 15:45:33 -0400

    feature: add cron to message channel every 10 seg

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,15 @@
 const { Events } = require("discord.js");
+const cron = require("cron");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
+    let scheduledMessage = new cron.CronJob("*/10 * * * * *", () => {
+      // This runs every day at 10:30:00, you can do anything you want
+      const channel = client.channels.cache.get("903474270653534210");
+      channel.send(eval(`hola cada 10 segundos`));
+    });
+    scheduledMessage.start();
   },
 };
