c2bc3d2e803bda8cfaee42eb20e6d7cdb654b7ca
Eduardo Luis Gonzalez <eduardoluis.gonzalez@leniolabs.com>
2023-05-19 10:01:28 -0300

    🛠  Little fix

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute() {
-		console.log('Ready ✅');
+		console.log('Ready 🚀');
 		// let scheduledMessage = new cron.CronJob("*/10 * * * * *", () => {
 		// This runs every day at 10:30:00, you can do anything you want
 		// const channel = client.channels.cache.get("903474270653534210");
