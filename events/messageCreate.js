35dd41f03fc29aada4aea6d10ee139ef2ab8d23b
Eduardo Luis Gonzalez <eduardoluis.gonzalez@leniolabs.com>
2023-05-19 09:44:25 -0300

    🐛 Fixed all eslint issues

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,23 +1,22 @@
-const { Events } = require("discord.js");
-const { clientId, mentionLeni } = require("../config.json");
+const { Events } = require('discord.js');
+const { clientId, mentionLeni } = require('../config.json');
 
 module.exports = {
-  name: Events.MessageCreate,
-  async execute(message) {
-    // console.log(`me mencionaron`, message);
-    console.log(`🔧 me`, message.content);
-    if (
-      !message.content.slice(0, 23).includes(`<@${clientId}`) &&
+	name: Events.MessageCreate,
+	async execute(message) {
+		// console.log(`me mencionaron`, message);
+		console.log('🔧 me', message.content);
+		if (
+			!message.content.slice(0, 23).includes(`<@${clientId}`) &&
       !message.content.slice(0, 23).includes(`<@&${mentionLeni}`)
-    )
-      return false;
-    return message.channel?.send("Loading...").then(async (loadingMessage) => {
-      const res = await fetch(
-        "https://run.mocky.io/v3/7387588a-565e-46ba-b032-8e44f4189a2f?mocky-delay=2000ms"
-      );
-      const data = await res?.json();
-      loadingMessage?.edit(`Hello, ${data.response}!`);
-    });
-  },
+		) {return false;}
+		return message.channel?.send('Loading...').then(async (loadingMessage) => {
+			const res = await fetch(
+				'https://run.mocky.io/v3/7387588a-565e-46ba-b032-8e44f4189a2f?mocky-delay=2000ms',
+			);
+			const data = await res?.json();
+			loadingMessage?.edit(`Hello, ${data.response}!`);
+		});
+	},
 };
 
--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,16 @@
-const { Events } = require("discord.js");
+const { Events } = require('discord.js');
 // const cron = require("cron");
 
 module.exports = {
-  name: Events.ClientReady,
-  once: true,
-  execute(client) {
-    console.log("ready ✅");
-    // let scheduledMessage = new cron.CronJob("*/10 * * * * *", () => {
-    // This runs every day at 10:30:00, you can do anything you want
-    // const channel = client.channels.cache.get("903474270653534210");
-    // channel.send(eval(`hola cada 10 segundos`));
-    // });
-    // scheduledMessage.start();
-  },
+	name: Events.ClientReady,
+	once: true,
+	execute() {
+		console.log('Ready ✅');
+		// let scheduledMessage = new cron.CronJob("*/10 * * * * *", () => {
+		// This runs every day at 10:30:00, you can do anything you want
+		// const channel = client.channels.cache.get("903474270653534210");
+		// channel.send(eval(`hola cada 10 segundos`));
+		// });
+		// scheduledMessage.start();
+	},
 };
